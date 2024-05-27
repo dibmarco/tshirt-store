0ce45187a0066dd3331d07e0727b16566e4ffacf
Marco Dib <129765795+dibmarco@users.noreply.github.com>
2024-05-26 23:00:40 -0300

    Update jsx and css files

--- rock-tshirts/src/App.jsx
+++ rock-tshirts/src/App.jsx
@@ -1,5 +1,67 @@
+const shirts = [
+  {
+    id: 1,
+    band: "Faith No More",
+    color: "black",
+  },
+
+  {
+    id: 2,
+    band: "Metallica",
+    color: "black",
+  },
+
+  {
+    id: 1,
+    band: "Motorhead",
+    color: "black",
+  },
+
+  /* {
+    id: 2,
+    band: "Anthrax",
+    color: "black",
+  }, */
+];
+
 function App() {
-  return <h1>Rock T-Shirts</h1>;
+  return (
+    <div className="content-wrapper">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Rock T-Shirts</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      {shirts.map((shirt) => <div className="shirt-container" key={shirt.id}>{shirt.band}</div>)}
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Social Media</p>
+    </footer>
+  );
 }
 
 export default App;
